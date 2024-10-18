1784ead05852032e3976f127e7d6e1cd37c20b73
Dalton Neres <145723466+neresdaltonn@users.noreply.github.com>
2024-10-18 12:25:00 -0300

    perguntas

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Linguagem',
+    'O que é uma linguagem de programação e para que serve?',
+    'É um sistema formal que permite aos programadores criar programas por meio de um conjunto da mescla de palavras-chave, símbolos, termos e comandos. Isso envolve a criação de programas por meio da especificação de ações, ordens, algoritmos e manipulação de dados.'
+)
+
+criaCartao(
+    'Linguagens',
+    'Quais são os principais tipos de linguagens de programação?',
+    'JavaScript, Python, Java, CSS e HTML'
+)
+
+criaCartao(
+    'Programação',
+    'Quais são os primeiros passos para aprender a programar?',
+    'O primeiro passo é aprender a lógica de programação.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é o HTML e para que ele serve?',
+    'É é o bloco de construção mais básico da web. Define o significado e a estrutura do conteúdo da web.'
+)
