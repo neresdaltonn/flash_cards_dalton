d4d62700a3f270ac5a7d1caa8498105c462116ec
Dalton Neres <145723466+neresdaltonn@users.noreply.github.com>
2024-10-25 08:54:41 -0300

    25-10-2024

--- aap.js
+++ aap.js
@@ -14,16 +14,6 @@ function criaCartao(categoria, pergunta, resposta) {
     </div>
     </div>
     `
-
-    let respostaEstaVisivel = false
-
-    function viraCartao() {
-        respostaEstaVisivel = !respostaEstaVisivel
-        cartao.classList.toggle('active', respostaEstaVisivel)
-    }
-    cartao.addEventListener('click', viraCartao)
-
-
     container.appendChild(cartao)
 
 }
