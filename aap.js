d4d22cacf7da602ec611d5b65b32f6c46a97c7aa
Dalton Neres <145723466+neresdaltonn@users.noreply.github.com>
2024-10-30 10:13:36 -0300

    30-10-2024

--- aap.js
+++ aap.js
@@ -1,10 +1,10 @@
-function criaCartao(categoria, pergunta, resposta){
+function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-  
+
     cartao.innerHTML = `
-    <div class="cartao__conteudo">
+                    <div class="cartao__conteudo">
                       <h3>${categoria}</h3>
                       <div class="cartao__conteudo__pergunta">
                           <p>${pergunta}</p>
@@ -12,7 +12,17 @@ function criaCartao(categoria, pergunta, resposta){
                       <div class="cartao__conteudo__resposta">
                           <p>${resposta}</p>
                       </div>
-                  </div>
+                    </div> 
+                    `
+                  
+let respostaEstaVisivel = false;
+
+function viraCartao(){
+    respostaEstaVisivel = !respostaEstaVisivel;
+    cartao.classList.toggle('active'), respostaEstaVisivel);
+}
+
+cartao.addEventListener('click',viraCartao);
     `
     container.appendChild(cartao);
-  }
+}
